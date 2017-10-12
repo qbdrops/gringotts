ed5d7d270b46c174c148bcdfc6a804d896710074
TideiSun <tideisun@TideiSuns-MacBook-Pro.local>
2017-10-12 15:48:39 +0800

    Add value in deploy sidechain contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 var IFC = artifacts.require('./IFC.sol');
 var SideChain = artifacts.require('./SideChain.sol');
-var scid = "0xabc",
-	poe = '0xe0936cc9130e93c272485df786d172902ecf048d44767e3ceb7add67be0b9a6f',
+var scid = "0x63d7db5ce060b288ecf5390594d5969bc1a206ceeb24df31cffcc8876df5e44b",
+	poe = '0x322e6f92b44ae32152e15f6d2f9469438c6bb353bdb6f2d220254bbd014f06bf',
     treeH = 3;
 
 module.exports = function(deployer) {
     deployer.deploy(IFC);
-    deployer.deploy(SideChain, scid, poe, treeH);
+    deployer.deploy(SideChain, scid, poe, treeH, {value:400});
 };
