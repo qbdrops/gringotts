87e364f7e177c237e877d3d8177ab26b155af351
Jerry.Jheng <jerry.jheng@isuncloud.com>
2017-10-20 16:43:51 +0800

    add somedepiction

--- test/testMerkleTree.js
+++ test/testMerkleTree.js
@@ -7,7 +7,7 @@ const MerkleTree = require('../indexMerkleTree/MerkleTree.js');
 const should = require('should');
 const keccak256 = require('js-sha3').keccak256;
 const clientAudit = require('../indexMerkleTree/clientAudit.js');
-let node = new Array(8) // a[0] not use , only use a[1-7]
+let node = new Array(8) //testArray :simulated merkletree of height 3 have 7 nodes
 let tree = new MerkleTree(3);
 tree.setSCID(1);
 
@@ -16,10 +16,10 @@ describe('#MerkleTree test', function(){
         // Arrange
         RH = tree.getRootHash();
         // Act
-        for(let i = 4 ; i < 8 ; i++){// a[4-7] : leaf
+        for(let i = 4 ; i < 8 ; i++){// put hash to initial leaf nodes
             node[i] = keccak256('initial no data');
         }
-        for(let i = 3 ; i > 0 ; i--){ 
+        for(let i = 3 ; i > 0 ; i--){// put hash to initial internal nodes 
             node[i] = keccak256(node[i * 2].concat(node[i * 2 + 1]));
         }
         // Assert
@@ -33,18 +33,18 @@ describe('#MerkleTree test', function(){
             'contentUser': 'hello world',
             'contentCp': 'hello world'
         };
-        // Act
-        let index = parseInt(keccak256(order.tid.toString()).substring(0, 12), 16);
-        let leafLocation =(1 << (3 - 1)) + Math.abs(index) % (1 << (3 - 1));
-        let tOrderHash = keccak256(order.contentUser.concat(order.contentCp));
-        let leafNodeHash = keccak256(tOrderHash);
-        node[leafLocation] = leafNodeHash;
         tree.putTransactionInTree(order);
-        for(let i = 3 ; i > 0 ; i--){ // internal node update
+        RH = tree.getRootHash();
+        // Act
+        let subHash = parseInt(keccak256(order.tid.toString()).substring(0, 12), 16);
+        let leafLocation =(1 << (3 - 1)) + Math.abs(subHash) % (1 << (3 - 1));
+        let orderHash = keccak256(order.contentUser.concat(order.contentCp));
+        let leafNodeHash = keccak256(orderHash); // compute orderHash to leafNodeHash
+        node[leafLocation] = leafNodeHash;
+        for(let i = 3 ; i > 0 ; i--){ //simulated merkletree's internal node update
             node[i] = keccak256(node[i * 2].concat(node[i * 2 + 1]));
         }
         treeLeafHash = tree.getNodeHash(order.tid);
-        RH = tree.getRootHash();
         // Assert
         treeLeafHash.should.equal(node[leafLocation]);// check leaf hash
         RH.should.equal(node[1]);// check root hash
@@ -56,18 +56,18 @@ describe('#MerkleTree test', function(){
             'contentUser': 'hello world',
             'contentCp': 'hello world'
         };
-        // Act
-        let index = parseInt(keccak256(order.tid.toString()).substring(0, 12), 16);
-        let leafLocation =(1 << (3 - 1)) + Math.abs(index) % (1 << (3 - 1));
-        let tOrderHash = keccak256(order.contentUser.concat(order.contentCp));
-        let leafNodeHash = keccak256(tOrderHash.concat(tOrderHash));
-        node[leafLocation] = leafNodeHash;
         tree.putTransactionInTree(order);
-        for(let i = 3 ; i > 0 ; i--){ // internal node update
+        RH = tree.getRootHash();
+        // Act
+        let subHash = parseInt(keccak256(order.tid.toString()).substring(0, 12), 16);
+        let leafLocation =(1 << (3 - 1)) + Math.abs(subHash) % (1 << (3 - 1));
+        let orderHash = keccak256(order.contentUser.concat(order.contentCp));
+        let leafNodeHash = keccak256(orderHash.concat(orderHash)); // compute orderHash to leafNodeHash
+        node[leafLocation] = leafNodeHash;
+        for(let i = 3 ; i > 0 ; i--){ //simulated merkletree's internal node update
             node[i] = keccak256(node[i * 2].concat(node[i * 2 + 1]));
         }
         treeLeafHash = tree.getNodeHash(order.tid);
-        RH = tree.getRootHash();
         // Assert
         treeLeafHash.should.equal(node[leafLocation]);// check leaf hash
         RH.should.equal(node[1]);// check root hash
@@ -82,17 +82,19 @@ describe('#ClientAudit test', function(){
             'contentCp': 'hello world'
         };
         let orderHash = keccak256(order.contentUser.concat(order.contentCp));
-        let slice = tree.extractSlice('0x123');
-        let hashSet = tree.getTransactionHashSet('0x123');
-        let location = tree.calcLeafIndex('0x123');
+        let slice = tree.extractSlice('0x123');// get slice which tid = 0x123
+        let hashSet = tree.getTransactionHashSet('0x123');// get hsahset which below that slice
+        let location = tree.calcLeafIndex('0x123');// get transaction's slice location
 
         let left;
         let right;
         let testSlice = new Array();
         let testSet = new Array();
-        // Act
+        // Act        
+        // push orderHash to testSet for simulating the real hashSet 
         testSet.push(orderHash);
         testSet.push(orderHash);
+        //get the audit node from simulation tree for simulating the real slice 
         for (;location > 1; location >>= 1) { 
             left = right = node[location];
             if (location % 2 == 0) {
@@ -103,9 +105,7 @@ describe('#ClientAudit test', function(){
             testSlice.push(left);
             testSlice.push(right);
         }
-        testSlice.push(node[1]);
-        
-        
+        testSlice.push(node[1]);        
         // Assert
        let origin = clientAudit.auditSlice(slice, hashSet, order);
        let test = clientAudit.auditSlice(testSlice, testSet, order);
