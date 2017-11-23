3a84add40ba7df32de97814161af0a46f9e7c8af
Ben Lu <ben.lu@isuncloud.com>
2017-11-23 15:41:37 +0800

    Remove sidechain migration process.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
 var IFC = artifacts.require('./IFC.sol');
-var SideChain = artifacts.require('./SideChain.sol');
-var scid = "0x63d7db5ce060b288ecf5390594d5969bc1a206ceeb24df31cffcc8876df5e44b",
-	poe = '0x322e6f92b44ae32152e15f6d2f9469438c6bb353bdb6f2d220254bbd014f06bf',
-    treeH = 3;
 
 module.exports = function(deployer) {
     deployer.deploy(IFC);
-    deployer.deploy(SideChain, scid, poe, treeH, {value:400});
 };
