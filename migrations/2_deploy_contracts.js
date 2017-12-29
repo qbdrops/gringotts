ab6d7c19b444c6176b9809ffa60944ff1458228c
TideiSun <tideisun@TideiSuns-MacBook-Pro.local>
2017-12-29 17:32:13 +0800

    Fix Issue

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var IFC = artifacts.require('./IFC.sol');
-var SideChainTemplate = artifacts.require('./SideChainTemplate.sol');
 
 module.exports = function(deployer) {
     deployer.deploy(IFC);
-    deployer.deploy(SideChainTemplate);
 };
