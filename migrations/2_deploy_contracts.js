d9659e97c394a6851d1e2652880b0f5ebf35b79b
Ben Lu <ben.lu@isuncloud.com>
2018-03-16 17:09:27 +0800

    Modify argv.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,9 @@ var IFCFlexible = artifacts.require('./IFCFlexible.sol');
 var argv = require('minimist')(process.argv.slice(2));
 
 module.exports = function(deployer) {
-   if (!argv.contract === true || argv.contract == 'general') {
+   if (!argv.contractType === true || argv.contractType == 'general') {
        deployer.deploy(IFC, 100);
-   } else if (argv.contract == 'flexible') {
+   } else if (argv.contractType == 'flexible') {
        deployer.deploy(IFCFlexible, 100);
    }
 };
