382f62c9339200aac3b3570ba406dbaa56b5eae5
Juin Chiu <juin.chiu@infinitechain.io>
2018-05-09 18:54:27 +0800

    Refactor models

--- models/receipt.js
+++ models/receipt.js
@@ -47,9 +47,7 @@ class Receipt {
     this.lightTxHash = lightTx.lightTxHash;
     this.lightTxData = lightTx.lightTxData;
     this.receiptData = this._normalize(orderedReceiptData);
-    this.receiptHash = EthUtils.sha3(
-      Object.values(this.receiptData).reduce((acc, curr) => acc + curr, '')
-    ).toString('hex');
+    this.receiptHash = this._sha3(Object.values(this.receiptData).reduce((acc, curr) => acc + curr, ''));
     this.sig = receiptJson.sig;
   }
 
@@ -101,6 +99,10 @@ class Receipt {
       sig: this.sig
     };
     return json;
+  }
+
+  _sha3 (content) {
+    return EthUtils.sha3(content).toString('hex');
   }
 }
 
